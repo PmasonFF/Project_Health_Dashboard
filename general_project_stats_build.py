64fa8ca601c467b08fc99740b61c17472baf0054
PmasonFF <petermijo@bmts.com>
2019-06-17 13:57:23 -0400

    Add date time (UTC) for listing, and import the csv file to google drive

--- general_project_stats_build.py
+++ general_project_stats_build.py
@@ -4,8 +4,17 @@ import os
 import io
 import operator
 import sys
+from datetime import datetime
 import panoptes_client
 from panoptes_client import Panoptes, Project
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
 
 Panoptes.connect(username=os.environ['User_name'], password=os.environ['Password'])
 
@@ -21,6 +30,8 @@ project_listing.sort(key=operator.itemgetter(0))
 sorted_on_state = sorted(project_listing, key=operator.itemgetter(4))
 
 with io.open('out_proj_stats_approved.csv', 'w', encoding='cp1252', newline='') as out_file:
+    out_file.write("Listing as of  " + str(datetime.utcnow())[0:10] + '  at '
+                   + str(datetime.utcnow())[10:16] + '  UTC' + '\n' + '\n')
     out_file.write('project_id' + ',' + 'subjects_count' + ','
                    + 'retired_subjects_count' + ',' + 'launch_approved' + ',' + 'state' + ',' + 'display_name' + '\n')
     for line in sorted_on_state:
@@ -28,4 +39,6 @@ with io.open('out_proj_stats_approved.csv', 'w', encoding='cp1252', newline='') 
         out_file.write(str(line[0]) + ',' + str(line[1]) + ',' + str(line[2]) + ',' + str(line[3]) + ',' + str(
             line[4]) + ',' + str(line[5]) + '\n')
 
+content = open('out_proj_stats_approved.csv', 'r', encoding='latin-1').read()
+client.import_csv('1Wg2ZNDeDZpxEwmY97b5ppb3VtO2Qd9jYKgC7EF0A3yo', content)
 #  ____________________________________________________________________________________________________________________
