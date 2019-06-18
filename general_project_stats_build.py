8e35476af242b3e1a6682a9ed86e2ee5ba99a895
PmasonFF <petermijo@bmts.com>
2019-06-18 12:37:34 -0400

    Add activity and completeness to project details

--- general_project_stats_build.py
+++ general_project_stats_build.py
@@ -22,22 +22,22 @@ project_listing = []
 for project in Project.where(launch_approved=True):
     try:
         project_listing.append([int(project.id), project.subjects_count,
-                                project.retired_subjects_count, project.launch_approved,
+                                project.retired_subjects_count, project.activity, int(project.completeness * 100 + .5),
                                 project.state, project.display_name])
     except panoptes_client.panoptes.PanoptesAPIException:
         print(str(sys.exc_info()[1]))
 project_listing.sort(key=operator.itemgetter(0))
-sorted_on_state = sorted(project_listing, key=operator.itemgetter(4))
+sorted_on_state = sorted(project_listing, key=operator.itemgetter(5))
 
 with io.open('out_proj_stats_approved.csv', 'w', encoding='cp1252', newline='') as out_file:
     out_file.write("Listing as of  " + str(datetime.utcnow())[0:10] + '  at '
                    + str(datetime.utcnow())[10:16] + '  UTC' + '\n' + '\n')
     out_file.write('project_id' + ',' + 'subjects_count' + ','
-                   + 'retired_subjects_count' + ',' + 'launch_approved' + ',' + 'state' + ',' + 'display_name' + '\n')
+                   + 'retired_subjects_count' + ',' + 'activity' + ',' + 'completeness' + ',' + 'state' + ',' + 'display_name' + '\n')
     for line in sorted_on_state:
         print(line)
         out_file.write(str(line[0]) + ',' + str(line[1]) + ',' + str(line[2]) + ',' + str(line[3]) + ',' + str(
-            line[4]) + ',' + str(line[5]) + '\n')
+            line[4]) + ',' + str(line[5]) + ',' + str(line[6]) + '\n')
 
 content = open('out_proj_stats_approved.csv', 'r', encoding='latin-1').read()
 client.import_csv('1Wg2ZNDeDZpxEwmY97b5ppb3VtO2Qd9jYKgC7EF0A3yo', content)
