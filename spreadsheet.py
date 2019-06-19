7d58e006ef8a20b75437ea4b07e6bee3b270ef20
PmasonFF <petermijo@bmts.com>
2019-06-19 13:12:12 -0400

    Add gspread_formatting functionality to spreadsheet.py

--- spreadsheet.py
+++ spreadsheet.py
@@ -10,6 +10,7 @@ client = gspread.authorize(creds)
 
 # Find a workbook by name and open the first sheet
 # Make sure you use the right name here.
+# Also options to open with key or open with url
 # Need to share the folder where the file is with service account email address, don't need to specify path in code
 # sheet = client.open("out_proj_stats_approved").sheet1
 
@@ -31,7 +32,19 @@ client = gspread.authorize(creds)
 # sheet.update_cell(1, 1, "project_id")
 # print(sheet.cell(1, 1).value)
 #
-# # Or you can insert a row in the spreadsheet:
+# # Fetch a cell range
+# cell_list = sheet.range('A1:B7')
+# print(cell_list)
+#
+# # Update a range of cells
+# cell_list = sheet.range('G1:H5')
+# for cell in cell_list:
+#     cell.value = ''
+#
+# # Update in batch
+# sheet.update_cells(cell_list)
+#
+# # You can insert a row in the spreadsheet:
 # row = ["I'm","inserting","a","row","into","a,","Spreadsheet","with","Python"]
 # index = 1
 # sheet.insert_row(row, index)
@@ -52,18 +65,7 @@ client = gspread.authorize(creds)
 # your_data = pd.DataFrame(your_data)
 # your_data.to_csv('filename.csv')
 #
-# # Fetch a cell range
-# cell_list = sheet.range('A1:B7')
-# print(cell_list)
-#
-# # Update a range of cells
-# cell_list = sheet.range('G1:H5')
-# for cell in cell_list:
-#     cell.value = ''
-#
-# # Update in batch
-# sheet.update_cells(cell_list)
-#
+
 # # Create a blank spreadsheet  This places the spreadsheet in the root drive!
 # sh = client.create('A new spreadsheet')
 # sh.share('petermijo@bmts.com', perm_type='user', role='owner')
@@ -97,3 +99,25 @@ client = gspread.authorize(creds)
 #
 # content = open('out_proj_stats_approved.csv', 'r', encoding='latin-1').read()
 # client.import_csv(file.get('id'), content)
+#
+
+#
+# # Using gspread_formatting to detect and set cell formats
+# from gspread_formatting import *
+#
+# # Detect formats
+# print(get_user_entered_format(sheet, 'A1'))
+#
+# # Set formats
+# fmt = cellFormat(
+#     backgroundColor=color(1, .9, .9),
+#     textFormat=textFormat(bold=True, foregroundColor=color(1, 0, 0)),
+#     horizontalAlignment='LEFT'
+#     )
+# format_cell_range(sheet, 'A1:J1', fmt)
+
+# # Useful details:
+#  colors = {'black': (0, 0, 0), 'white': (1, 1,1), 'red': (1, 0, 0), 'green': (0, 1, 0), 'blue': (0, 0, 1),
+#  'yellow': (1, 1, 0), 'cyan': (0, 1, 1), 'magenta': (1, 0, 1), 'orange': (1, .6, 0), 'purple': .6, 0, 1)}
+#  alignments = ['LEFT', 'CENTER', 'RIGHT']
+#  https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/cells#CellFormat
